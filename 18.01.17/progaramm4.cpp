7350bbc4535ddadbdca91768315d46690475920f
BudkovskyAlexander <arileev99@gmail.com>
2017-01-18 23:45:37 +0300

    Create progaramm4.cpp

---
+++ 18.01.17/progaramm4.cpp
@@ -0,0 +1,41 @@
+using namespace std;
+
+void outer_points(int n, int m);
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	outer_points(n, m);
+
+}
+
+void outer_points(int n, int m)
+{
+	int *prinadlegnost = new int[m];			
+	int *point = new int[m];
+	int *a = new int[n];
+	int *b = new int[n];
+	for (int i = 0; i <n; i++)							
+	{
+		cin >> a[i] >> b[i];
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> point[i];
+		prinadlegnost[i] = 0;
+	}
+	for (int i = 0; i < m; i++)													
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (point[i] >= a[j] && point[i] <= b[j])	prinadlegnost[i] ++;
+		}
+	}
+	for (int i = 0; i < m; i++)													
+	{
+		cout << n - prinadlegnost[i] << " ";
+	}
+   return 0;
+}
